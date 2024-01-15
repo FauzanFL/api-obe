6e8a4d12891d2f55f841a813ae5934a9bc1833d0
Fauzan FL <fauzanarth@gmail.com>
2024-01-15 08:03:33 +0700

    Delete ploClo and add status in perancangan obe

--- model/perancanganObe.go
+++ model/perancanganObe.go
@@ -3,6 +3,7 @@ package model
 type PerancanganObe struct {
 	ID          int    `json:"id" gorm:"primary_key"`
 	Nama        string `json:"nama"`
+	Status      string `json:"status"`
 	KurikulumID int    `json:"kurikulum_id"`
 }
 
